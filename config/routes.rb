7a8f64e6643a20b9827ba51b82839307ecf0bb54
romualdas <romualdas.gircys@interstartas.lt>
2023-10-06 21:28:08 +0300

    created blog post model and page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "blog_posts#index"
+
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
 end
